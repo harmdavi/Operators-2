59546505693571ccbb652d034d3d33a74803a5b8
harmdavi <harmondrum@gmail.com>
2020-08-19 08:45:24 -0600

    Add project files.

---
+++ Operators1.vb
@@ -0,0 +1,22 @@
+﻿'David Harmon
+'RCET0265
+'Fall 2020
+'Operator2
+'Repo
+
+
+Module Operators1
+
+    Sub Main()
+        'Let's do math!
+        Console.WriteLine(2 + 2)
+        Console.ReadLine()
+        Console.WriteLine(2 - 2)
+        Console.ReadLine()
+        Console.WriteLine(2 / 2)
+        Console.ReadLine()
+        Console.WriteLine(2 * 3)
+        Console.ReadLine()
+    End Sub
+
+End Module
